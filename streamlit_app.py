b83537303d06842122591305a3b04cd8dc9a7739
Alberto Chen <ac11599@nyu.edu>
2026-06-17 21:37:30 +0000

    data preprocessing moved to colab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 # --- Package Imports ---
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # datasets
 # https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/43nn-pn8j/about_data
@@ -18,26 +15,10 @@ st.sidebar.title("NYC Restaurant Inspections 🌐")
 page = st.sidebar.selectbox("Select Page", [
                             "Introduction 🍽️"])
 
-@st.cache_data(ttl=86400)
-def load_data():
-    return pd.read_csv("https://data.cityofnewyork.us/resource/43nn-pn8j.csv?$limit=300000")
-df = load_data()
-
-@st.cache_data(ttl=86400)
-def load_data():
-    inspections = pd.read_csv("https://data.cityofnewyork.us/resource/43nn-pn8j.csv?$limit=300000")
-    rodents = pd.read_csv("https://data.cityofnewyork.us/resource/p937-wjvj.csv?$limit=3300000")
-    # income = pd.read_csv("YOUR_INCOME_DATASET_URL_HERE?$limit=300000")
-    return inspections, rodents
-
-inspections_df, rodents_df = load_data()
-
 # --- Introduction Page ---
 if page == "Introduction 🍽️":
 
     st.title("Introduction 🍽️")
 
-    print(inspections_df.shape)
-    print(rodents_df.shape)
 
 
